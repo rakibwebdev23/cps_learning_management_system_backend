d4022312ab5e3b9417b80128140b8764ced9081e
rakibwebdev23 <rh.rakibhasan365@gmail.com>
2026-08-29 01:20:24 +0600

    fix: prevent localhost fallback from overriding connectionString

--- config/database.ts
+++ config/database.ts
@@ -43,8 +43,18 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
 
     postgres: {
       client: 'postgres',
-      connection: {
+      connection: env('DATABASE_URL') ? {
         connectionString: env('DATABASE_URL'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+        },
+        schema: env('DATABASE_SCHEMA', 'public'),
+      } : {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
@@ -56,10 +66,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            false
-          ),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
